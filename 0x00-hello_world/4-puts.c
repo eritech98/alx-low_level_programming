a914e8c4e02beeec7c5f88a9585fdc20cf407a37
Erick Olando <Opiyoerick1998@gmail.com>
2023-07-13 17:36:18 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+*main -Aprogramme that prints a line with put unction
+*Return :0(success)
+*/
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
