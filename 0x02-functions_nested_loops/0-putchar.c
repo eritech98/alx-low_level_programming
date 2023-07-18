263a6ba77f77871599578e6950d9da1a74ff984a
Erick Olando <Opiyoerick1998@gmail.com>
2023-07-18 16:18:43 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
-	/**
-	 * main - Entry point of my codes
-	 * 
-	 * Return: 0
-	 */
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point of the program.
+ *
+ * Return: 0 on successful execution
+ */
 
 
 	int main(void)
