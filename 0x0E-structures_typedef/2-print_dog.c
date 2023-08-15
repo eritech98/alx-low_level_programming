da015943e886911372daf707ba30a39681e13279
Erick Olando <Opiyoerick1998@gmail.com>
2023-08-14 18:55:53 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 #include "dog.h"
 
 /**
- *print_dog - This function prints a struct dog
+ *print_dog - This function prints
  *@d: The struct dog to be printed
  */
 
@@ -12,11 +12,11 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d->erick == NULL)
-		d->erick = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->erick, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
