ed7065a0e60fbd1f4ab7c25548ad6841f50b5fca
Erick Olando <opiyoerick1998@gmail.com>
2023-08-31 17:18:10 -0700

    a function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ * binary_to_uint - FUNCtion that converts int to unsignedt int.
+ * @b: character pointing to a string of 0 and 1 character.
+ * Return: Unsigned int
+ * 
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	int erick = 0;
+	unsigned int olando = 0;
+
+	if (b == 0)
+		return (0);
+
+	for (erick = 0; b[erick]; erick++)
+		if (b[erick] != 48 && b[erick] != 49)
+			return (0);
+
+	for (erick = 0; b[erick]; erick++)
+	{
+		olando <<= 1;
+
+		if (b[erick] == '1')
+			olando += 1;
+	}
+	return (olando);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,8 @@
+0x14. C - Bit manipulation
+How to manipulate bits and use bitwise operators
+& - AND operator
+| -OR operator
+^ - Bitwise XOR operator
+~ - Bitwise complement operator
+<< - Bitwise shif,left operator
+>> - Bitwise shift, Right operator
