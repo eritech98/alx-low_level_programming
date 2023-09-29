cd44227e1faebb174f5f8ac47c4f1a89dd816393
Erick Opiyo Olando <Opiyoerick1998@students.uonbi.ac.ke>
2023-09-28 19:20:39 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -16,6 +16,7 @@ void print_binary(unsigned long int n);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 int get_endianness(void);
+int _atoi(const char *s);
 
 unsigned int binary_to_uint(const char *b);
 int set_bit(unsigned long int *n, unsigned int index);
