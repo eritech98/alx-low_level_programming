0c394498bff53cfc1e84950b785bc5fbb643e86f
Erick Olando <Opiyoerick1998@gmail.com>
2023-11-13 18:01:44 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -nv -P ../ https://github.com/CarolyneMutemi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/print.so
+wget -nv -P ../ https://github.com/eritech98/alx-low_level_programming/raw/master/0x18-dynamic_libraries/print.so
 export LD_PRELOAD=../print.so
